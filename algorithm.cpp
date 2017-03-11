49d4c17530c9f103a26c6dd0e1b06902b7e62bc8
Helena Bantulà i Fonts <helenabantula@MacBook-Helena.local>
2017-03-11 13:33:45 +0100

    Implemented interruptor + first ideas of post-filtering

--- algorithm.cpp
+++ algorithm.cpp
@@ -70,11 +70,11 @@ void maxim_heart_rate_and_oxygen_saturation(uint16_t *pun_ir_buffer, int32_t n_i
 *               Since this algorithm is aiming for Arm M0/M3. formaula for SPO2 did not achieve the accuracy due to register overflow.
 *               Thus, accurate SPO2 is precalculated and save longo uch_spo2_table[] per each an_ratio.
 *
-* \param[in]    *pun_ir_buffer           - IR sensor data buffer
-* \param[in]    n_ir_buffer_length      - IR sensor data buffer length
-* \param[in]    *pun_red_buffer          - Red sensor data buffer
-* \param[out]    *pn_heart_rate          - Calculated heart rate value
-* \param[out]    *pch_hr_valid           - 1 if the calculated heart rate value is valid
+* \param[in]    *pun_ir_buffer            - IR sensor data buffer
+* \param[in]    n_ir_buffer_length        - IR sensor data buffer length
+* \param[in]    *!_buffer           - Red sensor data buffer
+* \param[out]    *pn_heart_rate           - Calculated heart rate value
+* \param[out]    *pch_hr_valid            - 1 if the calculated heart rate value is valid
 *
 * \retval       None
 */
